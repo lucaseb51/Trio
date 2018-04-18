b5ccf231229fd00684606216173a3629c6a0b907
Lucas G Eastburn <leastburn@mail.csuchico.edu>
2018-04-18 00:10:25 -0700

    Finished program

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+void greatestToLeast(int&, int&, int&);
+
 int main()
 {
   //DO NOT CHANGE WITHIN THIS AREA...
@@ -9,7 +11,7 @@ int main()
   cin>>red>>green>>blue;
   //...END OF "DO NOT CHANGE" AREA
 
-
+  greatestToLeast(red, green, blue);
 
 
 
@@ -19,3 +21,57 @@ int main()
   return 0;
   //...END OF "DO NOT CHANGE" AREA
 }
+
+
+void greatestToLeast(int &red, int &green, int &blue){
+    
+    int greatest=red;
+    int mid=green;
+    int least=blue;
+
+    if(red>blue&&red>green){
+        greatest=red;
+        if(blue>green){
+            mid=blue;
+            least=green;
+        }else{
+            mid=green;
+            least=blue;
+        }
+        
+    }else if(blue>red&&blue>green){
+        greatest=blue;
+        if(red>green){
+            mid=red;
+            least=green;
+        }else{
+            mid=green;
+            least=red;
+        }
+    }else if(green>red&&green>blue){
+        greatest=green;
+        if(red>blue){
+            mid=red;
+            least=blue;
+        }else{
+            mid=blue;
+            least=red;
+        }
+    }
+
+    red=greatest;
+    green=mid;
+    blue=least;
+
+}
+
+
+
+
+
+
+
+
+
+
+
